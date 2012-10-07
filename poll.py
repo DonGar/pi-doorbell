b4d88ae00658d36003818453c85c1725fc40a871
Don Garrett <dgarrett@acm.org>
2012-10-07 14:15:14 -0700

    Invoke a URL when the button is pushed.
    Small code cleanup as well.

--- poll.py
+++ poll.py
@@ -1,29 +1,42 @@
 #!/usr/bin/python
 
 import time
+import urllib
 
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
+def button_pushed():
+  print "Button down!"
+  f = urllib.urlopen("http://www:8080/doorbell")
+  f.readlines()
+  f.close()
 
-GPIO.setup(0, GPIO.IN)
+def main():
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(0, GPIO.IN)
 
-button_down = False
+  button_down = False
 
-while True:
-  # Gpio 0 is True for button up
-  read_down = not GPIO.input(0)
-  if read_down != button_down:
-    button_down = read_down
-    if button_down:
-      print "Button down!"
+  while True:
+    # Gpio 0 is True for button up
+    read_down = not GPIO.input(0)
+  
+    # If the button state changed
+    if read_down != button_down:
+      button_down = read_down
+    
+      # If the button when down, notify of a doorbell events
+      if button_down:
+        button_pushed()
 
-  time.sleep(0.05)
+    time.sleep(0.05)
 
-
+# TODO: when the Raspbian kernel supports GPIO events correctly...
 
 # GPIO.set_falling_event(0)
-
 # while True:
 #   time.sleep(1)
 #   print GPIO.event_detected(0)
+
+if __name__ == "__main__":
+    main()
